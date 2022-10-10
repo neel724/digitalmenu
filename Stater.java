88b26dcadabd70a8d10f78b69ce16291cdd49481
neel724 <113029762+neel724@users.noreply.github.com>
2022-10-10 14:48:10 +0530

    Add files via upload

---
+++ Stater.java
@@ -0,0 +1,14 @@
+package com.example.digitalmanu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Stater extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stater);
+    }
+}
