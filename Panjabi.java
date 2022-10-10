c14d15f061c8c36ce30748e85aff7d474a06d680
neel724 <113029762+neel724@users.noreply.github.com>
2022-10-10 14:49:02 +0530

    Add files via upload

---
+++ Panjabi.java
@@ -0,0 +1,14 @@
+package com.example.digitalmanu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Panjabi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_panjabi);
+    }
+}
